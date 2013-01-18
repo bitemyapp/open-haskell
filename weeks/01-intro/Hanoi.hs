dc54abb6db8da43c60e193ae55e696d6f5d5f34b
Brent Yorgey <byorgey@cis.upenn.edu>
2013-01-18 17:10:03 -0500

    use diagrams-postscript backend instead of cairo, for building on OS X

--- weeks/01-intro/Hanoi.hs
+++ weeks/01-intro/Hanoi.hs
@@ -1,9 +1,6 @@
 module Hanoi where
 
-import Diagrams.Backend.Cairo
 import Diagrams.Prelude
-
-type DC = Diagram Cairo R2
 
 colors = [blue, green, red, yellow, purple]
 
@@ -14,13 +11,11 @@ type Move  = (Int,Int)
 
 postHeight = 6
 
-renderDisk :: Disk -> DC
 renderDisk n = rect (fromIntegral n + 2) 1
                # lc black
                # fc (colors !! n)
                # lw 0.1
 
-renderStack :: Stack -> DC
 renderStack s = disks `atop` post
   where disks = (vcat . map renderDisk $ s)
                 # alignB
@@ -29,7 +24,6 @@ renderStack s = disks `atop` post
                 # fc saddlebrown
                 # alignB
 
-renderHanoi :: Hanoi -> DC
 renderHanoi h = view ((-7) & 0) ((fromIntegral (length h + 1) * 7) & postHeight)
               . hcat' with {catMethod = Distrib, sep = 7}
               . map renderStack
