9e1b385a920923b042bf94669d100bba76c8cfe8
Brent Yorgey <byorgey@cis.upenn.edu>
2013-02-14 12:08:01 -0500

    week 6

--- web/hakyll.hs
+++ web/hakyll.hs
@@ -14,7 +14,7 @@ import Hakyll
 -- all available lecture notes + HWs, not just those in the past.  Be
 -- sure to rm -rf web/_cache before rebuilding.
 filterLecs = True
-curWeek = 5
+curWeek = 6
 
 main = do
   today <- getCurrentDate
